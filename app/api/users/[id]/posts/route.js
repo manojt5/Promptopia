bc05cde05814f7cfcc43cdaeabff7b8b50e15f32
manojt5 <skamtbr@gmail.com>
2024-03-10 00:08:51 +0530

    'first'

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,12 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+export const GET=async(req,{params})=>{
+    try {
+        await connectToDB()
+        const prompts=await Prompt.find({creater:params.id}).populate('creater');
+        return new Response(JSON.stringify(prompts),{status:200})
+    } catch (error) {
+        return new Response("failed to fetch",{status:500})
+    }
+    
+}
