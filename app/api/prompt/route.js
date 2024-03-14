3fc110521be35afcb9ca24d359af37448b7e3dab
manojt5 <skamtbr@gmail.com>
2024-03-14 11:11:54 +0530

    check

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,5 +1,6 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+export const revalidate = 0;
 export const GET=async(req)=>{
     try {
         await connectToDB()
